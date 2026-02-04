82a6391fc79e0fccf3033f7927613334a4f5de56
Korneliusz Rabczak <kornel.rabczak@gmail.com>
2026-02-04 14:03:10 +0100

    refactor: Remove `EmbeddingModel` dependency and direct embedding generation from `InMemoryEmbeddingsService`.

--- core/src/main/java/com/thecookiezen/archiledger/infrastructure/embeddings/InMemoryEmbeddingsService.java
+++ core/src/main/java/com/thecookiezen/archiledger/infrastructure/embeddings/InMemoryEmbeddingsService.java
@@ -4,10 +4,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.ai.document.Document;
-import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 
@@ -17,19 +14,14 @@ import com.thecookiezen.archiledger.domain.repository.EmbeddingsService;
 @Service
 public class InMemoryEmbeddingsService implements EmbeddingsService {
 
-    private final Logger logger = LoggerFactory.getLogger(InMemoryEmbeddingsService.class);
-    private final EmbeddingModel embeddingModel;
     private final VectorStore vectorStore;
 
-    public InMemoryEmbeddingsService(EmbeddingModel embeddingModel, VectorStore vectorStore) {
-        this.embeddingModel = embeddingModel;
+    public InMemoryEmbeddingsService(VectorStore vectorStore) {
         this.vectorStore = vectorStore;
     }
 
     @Override
     public void generateEmbeddings(Entity entity) {
-        float[] embeddings = embeddingModel.embed(entity.observationsJoined());
-        logger.info("Generated embeddings for entity: {}", embeddings);
         vectorStore.add(List.of(
                 new Document(entity.name().toString(), entity.observationsJoined(), Collections.emptyMap())));
     }
